188b5a3bc9c91bb89217290384a74b44cd64b886
Rafael Larrea Sánchez <rlarreasanchez@gmail.com>
2023-11-12 08:44:38 +0100

    Se implementa autenticación a través de clerk

--- app/(auth)/(routes)/sign-in/page.tsx
+++
@@ -1,5 +0,0 @@
-const SignInPage = () => {
-  return <p>This is an unprotected Page</p>;
-};
-
-export default SignInPage;
---
+++ app/test/page.tsx
@@ -0,0 +1,5 @@
+const TestPage = () => {
+  return <p>This is an unprotected route</p>;
+};
+
+export default TestPage;
