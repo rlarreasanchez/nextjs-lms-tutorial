d9a197a84119b534afe8ce5b44c332a6b8445fc5
Rafael Larrea Sánchez <rlarreasanchez@gmail.com>
2023-11-12 09:36:52 +0100

    Se crea la estructura principal de la plantilla

---
+++ components/navbar-routes.tsx
@@ -0,0 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+
+const NavbarRoutes = () => {
+  return (
+    <div className="flex ml-auto gap-x-2">
+      <UserButton />
+    </div>
+  );
+};
+
+export default NavbarRoutes;
