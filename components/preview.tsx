77a3ee3975cefda2100378b452fb861b40ecf3b2
Rafael Larrea Sánchez <rlarreasanchez@gmail.com>
2023-11-15 15:42:28 +0100

    Se añade campo de checkbox para especificar si un capítulo es gratuito para previsualización

---
+++ components/preview.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useMemo } from "react";
+import dynamic from "next/dynamic";
+
+import "react-quill/dist/quill.bubble.css";
+
+interface PreviewProps {
+  value: string;
+}
+
+const Preview = ({ value }: PreviewProps) => {
+  const ReactQuill = useMemo(
+    () => dynamic(() => import("react-quill"), { ssr: false }),
+    []
+  );
+
+  return <ReactQuill theme="bubble" value={value} readOnly />;
+};
+
+export default Preview;
